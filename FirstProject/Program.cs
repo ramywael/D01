2341138c781e55d34ee50fc83a2dfbde884051de
ramywael <ramybakry9@gmail.com>
2024-11-20 22:41:40 +0200

    Add project files.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+           int value = 21;
+            Console.WriteLine(value);
+            char letter = 'R';
+            Console.WriteLine($"The Letter Is : {letter}"); //this is best for cpu (ELU) and is called string enterpolation
+                                                          
+            Console.WriteLine("The Letter Is : "+letter);    //Also it can be solved like this but is not a best solution
+
+
+            string? name=Console.ReadLine(); //This is how we read from c#
+            Console.WriteLine(name);
+
+
+            int value1= Convert.ToInt32(Console.ReadLine()); // if we need to read the value1 as  it is int we need to convert the string the user enter and then convert it to the identifier which is int 
+            Console.WriteLine(value1);
+
+        }
+    }
+}
---
+++ Islam's Carpet/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Islam_s_Carpet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint estimateValidation = 30;
+            uint pricePerSRoom = 25;
+            uint pricePerLRoom = 35;
+            Console.WriteLine("Number of small carpets:");
+            uint smallRoom=Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine("Number of large  carpets:");
+            uint bigRoom = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine( $"Price per small room :${pricePerSRoom}" );
+            Console.WriteLine($"Price per small room :${pricePerLRoom}");
+            uint cost = ((smallRoom * pricePerSRoom) + (bigRoom * pricePerLRoom));
+            Console.WriteLine($"The cost is : {cost}");
+            double tax = cost * 0.06;
+            Console.WriteLine($"Tax is :  ${tax}"  );
+            Console.WriteLine("===============================");
+            double totalEstimate = tax + cost;
+            Console.WriteLine($"Total estimate:{totalEstimate}");
+            Console.WriteLine($"This estimate is valid for {estimateValidation} days");
+
+        }
+    }
+}
